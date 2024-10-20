243fd3335391a395cd30dbeaf365911f6255c190
wangchifu <wangchifu@hdes.chc.edu.tw>
2024-10-20 21:58:04 +0800

    fix

--- resources/views/reviews/award.blade.php
+++ resources/views/reviews/award.blade.php
@@ -5,6 +5,13 @@
 @section('content')
 <section class="page-section" id="contact">
     <div class="container">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="{{ route('index') }}" class="text-decoration-none">首頁</a></li>
+              <li class="breadcrumb-item"><a href="{{ route('review.score') }}" class="text-decoration-none">分數與結果</a></li>
+              <li class="breadcrumb-item active" aria-current="page">{{ $group_name }}查閱結果</li>
+            </ol>
+          </nav>
         <div class="card" >
             <div class="card-body">
                 <h3 class="card-title">「{{ $report->title }}」{{ $group_name }}查閱結果</h3>
