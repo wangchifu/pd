2e90826d6fe8f9bef2aa62fa9f6dfd1505f3a3f7
wangchifu <wangchifu@hdes.chc.edu.tw>
2024-10-21 07:34:57 +0800

    fix

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -18,7 +18,7 @@
                 <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="{{ route('glogin') }}">登入</a></li>
                 @endguest
                 @auth
-                    @if(!empty(auth()->user()->school_name))                    
+                    @if(!empty(auth()->user()->school_name) and auth()->user()->school_name != "教育處" and auth()->user()->school_name != "網路中心")                    
                         <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="{{ route('fill.index') }}">學校填報</a></li>
                     @endif
                     <li class="nav-item dropdown mx-0 mx-lg-1">
