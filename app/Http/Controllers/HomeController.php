8b28c7220eb1d02006f1e819ccbb0d82cbc9bcbb
wangchifu <wangchifu@hdes.chc.edu.tw>
2025-11-03 09:07:54 +0800

    fix logout

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -200,10 +200,14 @@ class HomeController extends Controller
     {
         Auth::logout();
         
-        $url = "https://chc.sso.edu.tw/oidc/v1/logout-to-go";
-        $post_logout_redirect_uri = env('APP_URL');
-        $id_token_hint = session('id_token');
-        $link = $url . "?post_logout_redirect_uri=".$post_logout_redirect_uri."&id_token_hint=" . $id_token_hint;
+        if(empty(session('id_token'))){
+            $link = env('APP_URL');
+        }else{
+            $url = "https://chc.sso.edu.tw/oidc/v1/logout-to-go";
+            $post_logout_redirect_uri = env('APP_URL');
+            $id_token_hint = session('id_token');
+            $link = $url . "?post_logout_redirect_uri=".$post_logout_redirect_uri."&id_token_hint=" . $id_token_hint;
+        }        
         Session::flush();
         return redirect($link);
         
